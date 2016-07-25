d004e0b91c0c018fe78e007b15a179c20c824cf8
George Czabania <george.czabania@sparkventures.co.nz>
2016-07-25 21:49:17 +1200

    Add css extension to webpack prod config

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -50,7 +50,7 @@ module.exports = {
       'src',
       'node_modules',
     ],
-    extensions: ['', '.json', '.js', '.jsx'],
+    extensions: ['', '.json', '.js', '.jsx', '.css'],
   },
   plugins: [
     new CleanPlugin([assetsPath], {root: projectRootPath}),
